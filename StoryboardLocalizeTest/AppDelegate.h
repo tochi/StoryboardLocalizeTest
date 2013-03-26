e5a4c4aeadfecbb102e99b1e1cf66756d61c29e0
tochi <tochi@aguuu.com>
2013-03-26 09:43:44 +0900

    Initial Commit

---
+++ StoryboardLocalizeTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StoryboardLocalizeTest
+//
+//  Created by tochi on 2013/03/26.
+//  Copyright (c) 2013年 tochi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
